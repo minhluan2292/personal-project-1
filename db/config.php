8c99b5060b5bf1e1513da7de0ebc5c52cfc5011a
geniushcmus <geniushcmus@gmail.com>
2018-07-11 05:25:43 -0400

    edit function view mail

--- db/config.php
+++ db/config.php
@@ -8,7 +8,8 @@ $port='3306';
 
 try 
 {
-    $dbh = new PDO("mysql:host=$host;port=$port/;dbname=u276811496_tt",$username,$password);
+    //$dbh = new PDO("mysql:host=$host;port=$port/;dbname=u276811496_tt",$username,$password);
+    $dbh = new PDO("mysql:dbname=u276811496_tt;host=$host:$port;charset=utf8",$username,$password);
 
     //$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // <== add this line
 }
