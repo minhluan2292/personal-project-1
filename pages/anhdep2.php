84e8ac7703ef8faf11bbc6f466b1983b4ac2522f
minhluan2292 <minhluan2292@gmail.com>
2019-04-11 11:20:40 +0700

    Add files via upload

---
+++ pages/anhdep2.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title> Luan Huynh Album</title></title>
+
+<link rel="stylesheet" type="text/css" href="anhdep/styles.css" />
+
+</head>
+
+<body>
+
+<div id="slideshow">
+
+	<ul class="slides">
+	    <li><img src="anhdep/47.jpg" class="cover" alt="" /></li>
+	    <li><img src="anhdep/46.jpg" class="cover" alt="" /></li>
+	    <li><img src="anhdep/45.jpg" class="cover" alt="" /></li>
+	    <li><img src="anhdep/44.jpg" class="cover" alt="" /></li>
+	    <li><img src="anhdep/43.jpg" class="cover" alt="" /></li>
+	    <li><img src="anhdep/42.jpg" class="cover" alt="" /></li>
+	    <li><img src="anhdep/41.jpg" class="cover" alt="" /></li>
+	    <li><img src="anhdep/40.jpg" class="cover" alt="" /></li>
+	    <li><img src="anhdep/39.jpg" class="cover" alt="" /></li>
+	    <li><img src="anhdep/38.jpg" class="cover" alt="" /></li>
+	    <li><img src="anhdep/37.jpg" class="cover" alt="" /></li>
+	    <li><img src="anhdep/36.jpg" class="cover" alt="" /></li>
+	    <li><img src="anhdep/35.jpg" class="cover" alt="" /></li>
+	    <li><img src="anhdep/34.jpg" class="cover" alt="" /></li>
+	    <li><img src="anhdep/33.jpg" class="cover" alt="" /></li>
+	    <li><img src="anhdep/32.jpg" class="cover" alt="" /></li>
+	    <li><img src="anhdep/31.jpg" class="cover" alt="" /></li>
+	    <li><img src="anhdep/30.jpg" class="cover" alt="" /></li>
+	    <li><img src="anhdep/29.jpg" class="cover" alt="" /></li>
+	    <li><img src="anhdep/28.jpg" class="cover" alt="" /></li>
+			<li><img src="anhdep/27.jpg" class="cover" alt="" /></li>
+      <li><img src="anhdep/26.jpg" class="cover" alt="" /></li>
+			<li><img src="anhdep/22.jpg" class="cover" alt="" /></li>
+      <li><img src="anhdep/23.jpg" class="cover" alt="" /></li>
+			<li><img src="anhdep/24.jpg" class="cover" alt="" /></li>
+      <li><img src="anhdep/25.jpg" class="cover" alt="" /></li>
+			<li><img src="anhdep/21.jpg" class="cover" alt="" /></li>
+			<li><img src="anhdep/20.jpg" class="cover" alt="" /></li>
+    	<li><img src="anhdep/14.jpg" class="cover" alt="" /></li>
+      <li><img src="anhdep/13.jpg" class="cover" alt="" /></li>
+      <li><img src="anhdep/12.jpg" class="cover" alt="" /></li>
+      <li><img src="anhdep/11.jpg" class="cover" alt="" /></li>
+      <li><img src="anhdep/10.jpg" class="cover" alt="" /></li>
+      <li><img src="anhdep/9.jpg" class="cover" alt="" /></li>
+      <li><img src="anhdep/8.jpg" class="cover" alt="" /></li>
+      <li><img src="anhdep/7.jpg" class="cover" alt="" /></li>
+      <li><img src="anhdep/6.jpg" class="cover" alt="" /></li>
+      <li><img src="anhdep/5.jpg" class="cover" alt="" /></li>
+      <li><img src="anhdep/4.jpg" class="cover" alt="" /></li>
+      <li><img src="anhdep/3.jpg" class="cover" alt="" /></li>
+      <li><img src="anhdep/2.jpg" class="cover" alt="" /></li>
+      <li><img src="anhdep/1.jpg" class="cover" alt="" /></li>
+    </ul>
+
+    <span class="arrow previous"></span>
+    <span class="arrow next"></span>
+</div>
+
+
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+<script src="anhdep/script.js"></script>
+</body>
+</html>
